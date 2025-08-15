b1afb73ce36dd89d6c38bf7b73015b52d1428b2b
Joan Gomez <joangomezusma4@gmail.com>
2025-08-14 19:13:38 -0500

    Inicio desarrollo Api Saludos

---
+++ src/main/java/com/uniquindio/saludonombre/SaludoNombreApplication.java
@@ -0,0 +1,13 @@
+package com.uniquindio.saludonombre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaludoNombreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaludoNombreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniquindio/saludonombre/SaludoNombreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniquindio.saludonombre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaludoNombreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
